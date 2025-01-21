8835610e3d74f6d41683a17416ea9a57a8aecc2b
Brett Cherrington <brett.cherrington@adarga.ai>
2025-01-21 15:41:20 +0000

    feat: refactor to add new wrapper classes for special item types
    - add new Conjured implementation and alter texttest output to match requirements
    - add some documentation etc to help with maintenance

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,12 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+CONJURED = "Conjured Mana Cake"
 
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+MAX_ITEM_QUALITY = 50
+MIN_ITEM_QUALITY = 0
 
 
 class Item:
@@ -44,3 +17,128 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class SalesItem:
+    """ Generic class for items in the store.
+        - Quality decreases by 1 when the sell_in value is not expired
+        - Quality decreases by 2 when the sell_in value is expired
+        - Quality is never negative
+    """
+    STANDARD_DEGRADATION_RATE = 1
+
+    def __init__(self, item: Item, degradation_multiplier=1):
+        self.item = item
+        self.degradation_rate = degradation_multiplier * self.STANDARD_DEGRADATION_RATE
+        if self.item.name != SULFURAS and self.item.quality >= MAX_ITEM_QUALITY:
+            self.item.quality = MAX_ITEM_QUALITY
+
+    def _below_max_quality(self):
+        return self.item.quality < MAX_ITEM_QUALITY
+
+    def _above_min_quality(self):
+        return self.item.quality > MIN_ITEM_QUALITY
+
+    def _decrease_quality(self):
+        self.item.quality -= self.degradation_rate
+
+    def _increase_quality(self):
+        self.item.quality += self.degradation_rate
+
+    def _decrease_sell_in(self):
+        self.item.sell_in -= 1
+
+    def _is_expired(self):
+        return self.item.sell_in < 0
+
+    def update_quality(self):
+        self._decrease_sell_in()
+        if self._above_min_quality():
+            self._decrease_quality()
+        if self._is_expired() and self._above_min_quality():
+            self._decrease_quality()
+
+class AgedBrie(SalesItem):
+    """ Aged Brie increases in quality as it gets older.
+        - Quality increases by 1 when the sell_in value is not expired
+        - Quality increases by 2 when the sell_in value is expired
+        - Quality is never more than 50
+    """
+    def __init__(self, item: Item):
+        super().__init__(item)
+
+    def update_quality(self):
+        self._decrease_sell_in()
+        if self._below_max_quality():
+            self._increase_quality()
+        if self._is_expired() and self._below_max_quality():
+            self._increase_quality()
+
+class BackstagePasses(SalesItem):
+    """ Backstage passes increase in quality as the sell_in value approaches.
+        - Quality increases by 2 when there are 10 days or less
+        - Quality increases by 3 when there are 5 days or less
+        - Quality drops to 0 after the concert
+        - Quality is never more than 50
+    """
+
+    def __init__(self, item: Item):
+        super().__init__(item)
+
+    def update_quality(self):
+        self._decrease_sell_in()
+        if self._below_max_quality():
+            self._increase_quality()
+            # could pull these two expiry date numbers out to class variables
+            if self.item.sell_in < 10 and self._below_max_quality():
+                self._increase_quality()
+            if self.item.sell_in < 5 and self._below_max_quality():
+                self._increase_quality()
+        if self._is_expired():
+            self.item.quality = 0
+
+class Sulfuras(SalesItem):
+    """ Sulfuras never changes quality and never expires. """
+    SULFURAS_QUALITY = 80
+
+    def __init__(self, item: Item):
+        super().__init__(item)
+        # force the sulfuras quality to be 80
+        self.item.quality = self.SULFURAS_QUALITY
+
+    def update_quality(self):
+        pass
+
+class Conjured(SalesItem):
+    """ Conjured items degrade in quality twice as fast as normal items. """
+    def __init__(self, item: Item):
+        # "Twice as fast", could be interpreted as degrading by 2 each day or twice the standard rate
+        # This implementation assumes the latter
+        super().__init__(item, degradation_multiplier=2)
+
+
+def _get_item_wrapper(item: Item):
+    if item.name == AGED_BRIE:
+        return AgedBrie(item)
+    elif item.name == BACKSTAGE_PASSES:
+        return BackstagePasses(item)
+    elif item.name == SULFURAS:
+        return Sulfuras(item)
+    elif item.name == CONJURED:
+        return Conjured(item)
+    return SalesItem(item)
+
+
+class GildedRose(object):
+    """ Gilded Rose class
+
+        Maintains the items in the store and updates their quality.
+
+        During the update, the quality of the items is updated according to their type.
+    """
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            _get_item_wrapper(item).update_quality()
